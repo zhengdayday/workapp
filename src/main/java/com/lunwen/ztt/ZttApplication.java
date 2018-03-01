6ed06e34fa7347672b5e67a1c335463166c84786
daydayofzheng <1157790064@qq.com>
2018-03-01 18:33:34 +0800

    add ui-route

--- src/main/java/com/lunwen/ztt/ZttApplication.java
+++ src/main/java/com/lunwen/ztt/ZttApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZttApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZttApplication.class, args);
 	}
